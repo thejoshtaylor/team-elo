343cb66abbb9ddc130b13e68659c9c7a5d8a85e9
Joshua Taylor <theofficialjoshuataylor@gmail.com>
2024-09-05 12:59:24 -0700

    Added synergy table

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 # Josh Taylor - 2024
 
 import os
-import csv
+import signal
+import sqlite3
 from itertools import permutations, combinations
 
 # Defaults
@@ -10,33 +11,118 @@ START_ELO = 1000
 MIN_TEAM = 3
 MAX_TEAM = 13
 
-# Read in and parse the JSON file
-def get_data():
-    players = []
-    if not os.path.exists('players.csv'):
-        with open('players.csv', 'w') as f:
-            pass
-        return players
+# SQLite3 database
+conn = None
 
-    with open('players.csv', 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            if len(row) == 2:
-                players.append({'name': row[0], 'elo': int(row[1])})
-            else:
-                break
+
+# Create the database if it doesn't exist
+def create_db():
+    global conn
+    if conn is None:
+        conn = sqlite3.connect("players.db")
+    c = conn.cursor()
+    c.execute(
+        "CREATE TABLE IF NOT EXISTS players (id INTEGER PRIMARY KEY, name TEXT, elo INT)"
+    )
+    c.execute(
+        "CREATE TABLE IF NOT EXISTS synergy (sid INTEGER PRIMARY KEY, synergy INT)"
+    )
+    conn.commit()
+
+
+# Close the database
+def close_db():
+    global conn
+    if conn is not None:
+        conn.close()
+
+
+# Read in the database
+def get_data():
+    global conn
+    create_db()
+    c = conn.cursor()
+    c.execute("SELECT * FROM players")
+    players = c.fetchall()
+    # get column names
+    columns = [description[0] for description in c.description]
+    # convert to dictionary
+    players = [{col: player[i] for i, col in enumerate(columns)} for player in players]
     return players
 
-# Write the data to the JSON file
-def write_data(players):
-    print(f"Writing {len(players)} players")
-    with open('players.csv', 'w') as f:
-        writer = csv.writer(f, lineterminator='\n')
-        writer.writerows([(player['name'], player['elo']) for player in players])
 
+# Write the data to the database
+def write_data(players):
+    global conn
+    c = conn.cursor()
+    c.execute("DELETE FROM players")
+    c.executemany(
+        "INSERT INTO players VALUES (NULL, ?, ?)",
+        [(player["name"], player["elo"]) for player in players],
+    )
+    conn.commit()
+
+
+# Get all synergy data from the database
+def get_synergy_data():
+    global conn
+    create_db()
+    c = conn.cursor()
+    c.execute("SELECT * FROM synergy")
+    synergy = c.fetchall()
+    # get column names
+    columns = [description[0] for description in c.description]
+    # convert to dictionary
+    synergy = [{col: syn[i] for i, col in enumerate(columns)} for syn in synergy]
+    return synergy
+
+
+# Get synergyID
+def get_synergy_id(player1, player2):
+    player1, player2 = sorted([player1, player2])
+    return int(f"{player1:04d}{player2:04d}")
+
+
+# Get players from synergyID
+def get_players_from_synergy_id(synergy_id):
+    return int(f"{synergy_id:08d}"[:4]), int(f"{synergy_id:08d}"[4:])
+
+
+# Get synergy from database
+def get_synergy(player1, player2):
+    global conn
+    synergyID = get_synergy_id(player1, player2)
+    create_db()
+    c = conn.cursor()
+    c.execute("SELECT synergy FROM synergy WHERE sid = ?", (synergyID,))
+    synergy = c.fetchone()
+    if synergy is None:
+        return 0
+    return synergy[0]
+
+
+# Get synergy from array
+def get_synergy_from_data(synergy_data, player1, player2):
+    synergyID = get_synergy_id(player1, player2)
+    for s in synergy_data:
+        if s["sid"] == synergyID:
+            return s["synergy"]
+    return 0
+
+
+# Write synergy to database
+def write_synergy_data(player1, player2, synergy):
+    global conn
+    c = conn.cursor()
+    c.execute("UPSERT INTO synergy VALUES (?, ?)", (get_synergy_id(player1, player2), synergy))
+    conn.commit()
+
+
+# Print the title
 def title():
     print()
-    print(r"""
+    print(
+        r"""
 $$$$$$$$\                                      $$$$$$$$\ $$\           
 \__$$  __|                                     $$  _____|$$ |          
    $$ | $$$$$$\   $$$$$$\  $$$$$$\$$$$\        $$ |      $$ | $$$$$$\  
@@ -46,25 +132,28 @@ $$$$$$$$\                                      $$$$$$$$\ $$\
    $$ |\$$$$$$$\ \$$$$$$$ |$$ | $$ | $$ |      $$$$$$$$\ $$ |\$$$$$$  |
    \__| \_______| \_______|\__| \__| \__|      \________|\__| \______/ 
                                                                        
-""")
+"""
+    )
     print("Josh Taylor - 2024")
     print()
 
+
 def menu():
-    print('[v] View Players')
-    print('[n] New Player')
-    print('[r] Remove Player')
-    print('[u] Update Player')
-    print('[g] Generate Teams')
-    print('[q] Quit')
+    print("[v] View Players")
+    print("[n] New Player")
+    print("[r] Remove Player")
+    print("[u] Update Player")
+    print("[g] Generate Teams")
+    print("[q] Quit")
     print()
 
-    res = input('> ').lower()
-    while res not in ['v', 'n', 'r', 'u', 'g', 'q']:
-        print('Invalid choice')
-        res = input('> ').lower()
+    res = input("> ").lower()
+    while res not in ["v", "n", "r", "u", "g", "q"]:
+        print("Invalid choice")
+        res = input("> ").lower()
 
     return res
+
 
 # Generate the team sizes
 def generateTeamSizes(playerCount, MIN_SIZE=MIN_TEAM, MAX_SIZE=MAX_TEAM):
@@ -79,10 +168,11 @@ def generateTeamSizes(playerCount, MIN_SIZE=MIN_TEAM, MAX_SIZE=MAX_TEAM):
 
     return options
 
+
 def generateCombos(playerIndexes, team_sizes=[5, 5]):
     if team_sizes == []:
         return []
-    
+
     firstMember = None
     for combo in combinations(playerIndexes, team_sizes[0]):
         if firstMember is None:
@@ -98,6 +188,7 @@ def generateCombos(playerIndexes, team_sizes=[5, 5]):
             for subcombo in generateCombos(remaining, team_sizes[1:]):
                 yield [combo] + subcombo
 
+
 # Generate the team options
 def generateOptions(players, team_sizes=[(5, 5)]):
     options = []
@@ -107,22 +198,30 @@ def generateOptions(players, team_sizes=[(5, 5)]):
 
     # generate all possible team combinations then package them in a dictionary
     combos = generateCombos(list(range(len(players))), flat_teams)
+    synergy_data = get_synergy_data()
     for combo in combos:
         teams_dict = []
         for i, team in enumerate(combo):
-            entry = {"team_number": i + 1, "players": [players[player] for player in team], "elo": sum([players[player]['elo'] for player in team])}
+            entry = {
+                "team_number": i + 1,
+                "players": [players[player] for player in team],
+                "elo": sum([players[player]["elo"] for player in team])
+                + get_team_synergy(synergy_data, [players[player] for player in team]),
+            }
             teams_dict.append(entry)
         options.append(teams_dict)
 
     return options
 
+
 def getUniquenessOfTeams(team1, team2):
-    return len(set([player['name'] for player in team1+team2])) - len(team1)
+    return len(set([player["name"] for player in team1 + team2])) - len(team1)
+
 
 def getUniquenessOfOptions(option1, option2):
     # get lists of teams
-    teams1 = [team['players'] for team in option1]
-    teams2 = [team['players'] for team in option2]
+    teams1 = [team["players"] for team in option1]
+    teams2 = [team["players"] for team in option2]
 
     if len(teams1) != len(teams2):
         return len(teams1) + len(teams2)
@@ -133,10 +232,20 @@ def getUniquenessOfOptions(option1, option2):
     for p in permutations(teams):
         uniqueness = 0
         for i in range(len(teams)):
-            uniqueness += getUniquenessOfTeams(teams1[i], teams2[p[i]-1])
+            uniqueness += getUniquenessOfTeams(teams1[i], teams2[p[i] - 1])
         uniquenesses.append(uniqueness)
 
     return min(uniquenesses)
+
+
+# Get the synergy amongst a team
+def get_team_synergy(synergy_data, team):
+    synergy = 0
+    for i in range(len(team)):
+        for j in range(i + 1, len(team)):
+            synergy += get_synergy_from_data(synergy_data, team[i]["id"], team[j]["id"])
+    return synergy
+
 
 # Main function
 def main():
@@ -147,34 +256,37 @@ def main():
 
     while True:
         choice = menu()
-        if choice == 'v':
+        if choice == "v":
             print()
             for player in players:
                 print(f"{player['name']} ({player['elo']})")
             print()
-        elif choice == 'n':
-            name = input('Name: ')
-            players.append({'name': name, 'elo': START_ELO})
+        elif choice == "n":
+            name = input("Name: ")
+            players.append({"name": name, "elo": START_ELO})
             print(f"Added {name} with an ELO of {START_ELO}")
             print()
-        elif choice == 'r':
-            name = input('Name: ')
+            write_data(players)
+        elif choice == "r":
+            name = input("Name: ")
             for player in players:
-                if player['name'] == name:
+                if player["name"] == name:
                     players.remove(player)
                     print(f"Removed {name}")
                     break
             print()
-        elif choice == 'u':
-            name = input('Name: ')
+            write_data(players)
+        elif choice == "u":
+            name = input("Name: ")
             for player in players:
-                if player['name'] == name:
-                    new_elo = int(input('New ELO: '))
-                    player['elo'] = new_elo
+                if player["name"] == name:
+                    new_elo = int(input("New ELO: "))
+                    player["elo"] = new_elo
                     print(f"Updated {name} to {new_elo}")
                     break
             print()
-        elif choice == 'g':
+            write_data(players)
+        elif choice == "g":
             print()
 
             options = []
@@ -191,25 +303,41 @@ def main():
             #     for option in options:
             #         writer.writerow(sorted(','.join(sorted(player['name'] for player in team['players'])) for team in option))
 
-            options.sort(key=lambda x: sum([abs(x[i]['elo'] - x[i+1]['elo']) for i in range(0, len(x), 2)]))
+            options.sort(
+                key=lambda x: sum(
+                    [abs(x[i]["elo"] - x[i + 1]["elo"]) for i in range(0, len(x), 2)]
+                )
+            )
 
             currentOption = options[0]
             # print elo differences
             for j, option in enumerate(options):
                 x = 0
-                print(f"[{j+1:3d}]", end=' ')
+                print(f"[{j+1:3d}]", end=" ")
                 for i in range(0, len(option), 2):
-                    print(f"({option[i]['elo']} - {option[i + 1]['elo']})", end=' + ')
-                    x += abs(option[i]['elo'] - option[i + 1]['elo'])
-                print(f'\b\b= {x}', end='')
+                    print(f"({option[i]['elo']} - {option[i + 1]['elo']})", end=" + ")
+                    x += abs(option[i]["elo"] - option[i + 1]["elo"])
+                print(f"\b\b= {x}", end="")
                 print(f" ({getUniquenessOfOptions(currentOption, option)})")
                 if j >= 9:
                     break
 
             print()
-        elif choice == 'q':
-            write_data(players)
+        elif choice == "q":
             break
 
-if __name__ == '__main__':
+
+# Signal handler
+def signal_handler(sig, frame):
+    close_db()
+    print("SIGNAL RECEIVED: Exiting")
+    os._exit(0)
+
+
+# trigger signal handler on interrupt
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
+if __name__ == "__main__":
     main()
+    close_db()
